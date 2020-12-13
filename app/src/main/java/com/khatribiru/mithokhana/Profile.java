127c8002df63e34d180dc391f122dfdc83471e7b
khatrabiru <khatrabiru@gmail.com>
2020-12-13 17:19:34 +0000

    Mitho Khana: Add My profile UI with User's basic information.

--- app/src/main/java/com/khatribiru/mithokhana/Profile.java
+++ app/src/main/java/com/khatribiru/mithokhana/Profile.java
@@ -2,13 +2,28 @@ package com.khatribiru.mithokhana;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class Profile extends AppCompatActivity {
+
+    TextView my_profile;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        my_profile = findViewById(R.id.my_profile);
+
+        my_profile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Profile.this, MyProfile.class);
+                startActivity(intent);
+            }
+        });
     }
 }
